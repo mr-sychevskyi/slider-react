91b1877f7296cc26ca5895a4c0ac2f681b698e27
Nikolay Sychevskyi <n.sychevskyi@gmail.com>
2018-07-04 17:52:57 +0300

    Initial commit

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 import './style.scss';
 
-import logo from './logo-uptech.png';
-
 const Header = () => (
     <header className="header">
         <div className="header-logo">
-            <img src={logo} className="img-responsive" alt="logo-movie"/>
+			<i className="slideshow-icon material-icons">slideshow</i>
         </div>
         <h3 className="header-title">React Slider</h3>
     </header>
